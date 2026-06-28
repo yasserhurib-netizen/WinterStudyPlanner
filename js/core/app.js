5e3307bfcbfe53dd3c5ae899b1f54c81cb38ea7b
yasserhurib-netizen <yasserhurib@gmail.com>
2026-06-29 00:01:50 +0300

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -1,6 +1,9 @@
 import { loadData } from "./data.js";
+import { renderWeeks } from "../modules/weeks.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadData();
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadData();
+    renderWeeks();
+
     console.log("App Started 🚀");
 });
