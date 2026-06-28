2828b5d3d521ae082c57675da53f9a25955a98b8
yasserhurib-netizen <yasserhurib@gmail.com>
2026-06-29 00:14:36 +0300

    Create weekDetails.js

---
+++ js/modules/weekDetails.js
@@ -0,0 +1,20 @@
+import { state } from "../core/state.js";
+
+export function openWeek(id) {
+
+    const week = state.weeks.find(w => w.id === id);
+
+    if (!week) return;
+
+    let content = `📅 ${week.title}\n\n`;
+
+    if (week.lessons.length === 0) {
+        content += "لا توجد دروس بعد";
+    } else {
+        week.lessons.forEach(l => {
+            content += `- ${l.title} ${l.completed ? "✔" : "❌"}\n`;
+        });
+    }
+
+    alert(content);
+}
