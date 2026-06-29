b660c975500dfff300ff69e53e9f240924be0079
yasserhurib-netizen <yasserhurib@gmail.com>
2026-06-29 05:39:03 +0300

    Update weekCard.js

--- js/components/weekCard.js
+++ js/components/weekCard.js
@@ -1,18 +1,23 @@
-export function createWeekCard(week) {
+export function createWeekCard(week){
 
-    const card = document.createElement("div");
+    const card=document.createElement("div");
 
-    card.className = "card";
+    card.className="card";
 
-    card.innerHTML = `
-        <h3>${week.title}</h3>
+    card.innerHTML=`
+
+        <h3>📅 ${week.title}</h3>
 
         <p>التقدم ${week.progress}%</p>
 
         <div class="progress">
+
             <div style="width:${week.progress}%"></div>
+
         </div>
+
     `;
 
     return card;
+
 }
